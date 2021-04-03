d93a8311c2bc7b41eb3a317a18217cd044af5ebd
LaNice <lanicemp10@gmail.com>
2021-04-03 15:25:18 -0400

    created componet to import into userForm

---
+++ evrates-client/src/components/UserMonthlyBill.js
@@ -0,0 +1,19 @@
+import react from "react"
+
+const UserMonthlyBill =({handleElectricChange,electricBill} )=>{
+
+    return(
+        <div>
+        <h3>What is the cost of your current monthly electric bill? </h3>
+        <input
+          name='electic_bill'
+          type='number'
+          value={electricBill}
+          onChange={handleElectricChange}
+          placeholder='Enter Current Electric Bill'
+        />
+        </div>
+        
+    )
+}
+export default UserMonthlyBill
---
+++ evrates-client/src/components/UserRate.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+const UserRate = ({ handleRateChange, rate }) => {
+  //line 3 destructures handleRrateChange and HandleSubmit from props
+
+  return (
+    <div>
+      <h3>Which rate do you currently use?</h3>
+      <h3>Rate A .15 </h3>
+      <h3> Rate B</h3>
+      <select name='rate' value={rate} type='text' onChange={handleRateChange}>
+        <option value='RateA'>Rate A </option>
+        <option value='RateB'>Rate B</option>
+      </select>
+    </div>
+  );
+};
+export default UserRate;
