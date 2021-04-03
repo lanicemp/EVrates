2c95ebfd53a906fbe4f972aed062e768357b66e4
LaNice <lanicemp10@gmail.com>
2021-04-03 17:06:38 -0400

    created UserMiles component

---
+++ evrates-client/src/components/UserMiles.js
@@ -0,0 +1,45 @@
+import react from 'react'
+import styled from "styled-components";
+
+
+const StyledInputWrapper = styled.div`
+  justify-content: center;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+const StyledInput = styled.input`
+  background: #54c6eb;
+
+  border-radius: 10px;
+  align-content: center;
+`;
+
+const StyledBall = styled.div`
+  positon: absolute;
+  background: yellow;
+  border-radius: 100%;
+  height: 12px;
+  width: 12px;
+  border: 1px solid orange;
+`;
+
+const UserMiles = ({handleMileChange, miles})=>{
+    return(
+   <div>
+    <h3>How Many miles do you drive per year?</h3>
+    <StyledInputWrapper>
+      <StyledInput
+        name='miles'
+        value={miles}
+        type='range'
+        min='1000'
+        max='100000'
+        step='1000'
+        onChange={handleMileChange}
+      />
+    </StyledInputWrapper>
+    </div>
+    )
+}
+export default UserMiles
