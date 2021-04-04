c5e4b728ba5bb8b71cc9c9fc49b9ba890dc35c42
LaNice <lanicemp10@gmail.com>
2021-04-03 21:58:29 -0400

    updated logic and messages  for rate A and B

--- evrates-client/src/components/UserForm.js
+++ evrates-client/src/components/UserForm.js
@@ -9,8 +9,9 @@ import UserRate from "./UserRate";
 const UserForm = () => {
   const [rate, setRate] = useState("Rate A");
   const [miles, setMiles] = useState("20000");
-  const [timeOfUse, setTimeOfUse] = useState("Between midnight and 5am");
+  const [timeOfUse, setTimeOfUse] = useState("Between noon and 6pm");
   const [message_A, setMessageA] = useState("");
+  const [message_B, setMessageB] = useState("");
   const [electricBill, setElectricBill] = useState("");
 
   //   const [message_B, setMessageB] = (useState = "");
@@ -37,17 +38,40 @@ const UserForm = () => {
     e.preventDefault();
     let message_A = "Rate A is the Better Option";
     let message_B = "Rate B is the Better Option";
-    let bill_A = "";
+    
+    
+    let homeLoadA = 1350.56;
+    let homeLoadB = 1033.0;
+    let bill_A =(0.3 * 0.15 * miles) + homeLoadA;
+    let bill_B_high = (0.3 * .20 * miles) + homeLoadB ;
+    let bill_B_low = (0.3 * .08 * miles) + homeLoadB
+    //homeLoad per year was calculated by the sum of the Electricity:Facility column in the excel sheet then multiplied by the flat rate of $0.15/kwh
+    //the total is $1350.56 per year or $112.55per month
+
     // let homerate =
     //rate A is Flat $0.15/ kWh
     //rate B is TOU rate of &0.20/ kWh between noon and 6pm, and $0.08/ kWh otherwise
+    //homeloadB cost was calculated in excel creating a field to replicate the hours of use then multipling the sum of the high use hrs by $.20
+    //the total during high usage was 521.1727199 the total during low usage was 511.8280342.
+    //homeloadB= 1033.000754
+
     console.log(rate === "Rate A");
-    console.log(miles);
-    console.log(0.3 * 0.15 * miles);
+    console.log("miles", miles);
+    console.log("bill A ", 0.3 * 0.15 * miles + homeLoadA);
     if (rate === "Rate A") {
-      bill_A = 0.3 * 0.15 * miles;
-      console.log(bill_A);
-      return setMessageA(message_A);
+      console.log("Between noon and 6pm", timeOfUse === "Between noon and 6pm");
+      if (timeOfUse === "Between noon and 6pm") {
+
+        if(bill_A < bill_B_high){
+          return setMessageA(message_A)
+        }
+       return setMessageB(message_B)
+      }
+      if (timeOfUse === "Between 7pm and 11am") {
+        bill_A = 0.3 * 0.15 * miles + homeLoadA;
+      }
+
+      // return setMessageA(message_A);
     }
     //   return (setMessageB(message_B));
 
@@ -56,17 +80,14 @@ const UserForm = () => {
     console.log(timeOfUse);
     setRate("Rate A");
     setMiles("20000");
-    setTimeOfUse("Between midnight and 5am");
+    setTimeOfUse("Between noon and 6pm");
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <UserRate handleRateChange={handleRateChange} rate={rate} />
-        <UserMonthlyBill
-          handleElectricChange={handleElectricChange}
-          electricBill={electricBill}
-        />
+        
         <UserMiles handleMileChange={handleMileChange} miles={miles} />
         <UserTimeOfUse
           handleTOUChange={handleTOUChange}
@@ -76,7 +97,7 @@ const UserForm = () => {
       </form>
 
       <h2>{message_A}</h2>
-      {/* <h2>{message_B}</h2> */}
+      <h2>{message_B}</h2>
     </div>
   );
 };
