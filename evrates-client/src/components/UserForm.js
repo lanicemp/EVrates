d73b15f7b594010cb06b9cbedc639a61ca58d4d2
LaNice <lanicemp10@gmail.com>
2021-04-04 07:41:58 -0400

    updated conditionals to display user savings

--- evrates-client/src/components/UserForm.js
+++ evrates-client/src/components/UserForm.js
@@ -10,9 +10,9 @@ const UserForm = () => {
   const [rate, setRate] = useState("Rate A");
   const [miles, setMiles] = useState("20000");
   const [timeOfUse, setTimeOfUse] = useState("Between noon and 6pm");
-  const [message_A, setMessageA] = useState("");
-  const [message_B, setMessageB] = useState("");
-  const [electricBill, setElectricBill] = useState("");
+  const [message, setMessage] = useState("");
+  const [difference, setDifference] = useState("");
+  const [savings, setSavings] = useState("");
 
   //   const [message_B, setMessageB] = (useState = "");
 
@@ -30,21 +30,21 @@ const UserForm = () => {
     setTimeOfUse(value);
   };
 
-  const handleElectricChange = (e) => {
-    const { value } = e.target;
-    setElectricBill(value);
-  };
+  // const handleElectricChange = (e) => {
+  //   const { value } = e.target;
+  //   setElectricBill(value);
+  // };
   const handleSubmit = (e) => {
     e.preventDefault();
-    let message_A = "Rate A is the Better Option";
+    let message_A = "Rate A is the Better Option.";
     let message_B = "Rate B is the Better Option";
-    
-    
+    let savings= "Savings of!"
+    let difference = "";
     let homeLoadA = 1350.56;
     let homeLoadB = 1033.0;
-    let bill_A =(0.3 * 0.15 * miles) + homeLoadA;
-    let bill_B_high = (0.3 * .20 * miles) + homeLoadB ;
-    let bill_B_low = (0.3 * .08 * miles) + homeLoadB
+    let bill_A = 0.3 * 0.15 * miles + homeLoadA;
+    let bill_B_high = 0.3 * 0.2 * miles + homeLoadB;
+    let bill_B_low = 0.3 * 0.08 * miles + homeLoadB;
     //homeLoad per year was calculated by the sum of the Electricity:Facility column in the excel sheet then multiplied by the flat rate of $0.15/kwh
     //the total is $1350.56 per year or $112.55per month
 
@@ -61,17 +61,36 @@ const UserForm = () => {
     if (rate === "Rate A") {
       console.log("Between noon and 6pm", timeOfUse === "Between noon and 6pm");
       if (timeOfUse === "Between noon and 6pm") {
-
-        if(bill_A < bill_B_high){
-          return setMessageA(message_A)
+        if (bill_A < bill_B_high) {
+          difference = bill_B_high - bill_A
+          return setMessage(message_A), setDifference(difference), setSavings(savings);
         }
-       return setMessageB(message_B)
+        difference = bill_A - bill_B_high
+        return setMessage(message_B), setDifference(difference), setSavings(savings);
       }
       if (timeOfUse === "Between 7pm and 11am") {
-        bill_A = 0.3 * 0.15 * miles + homeLoadA;
+        if (bill_A < bill_B_low) {
+          difference = bill_B_low - bill_A
+          return setMessage(message_A), setDifference(difference), setSavings(savings);
+        }
+        difference = bill_A - bill_B_low
+        return setMessage(message_B), setDifference(difference), setSavings(savings)
       }
-
       // return setMessageA(message_A);
+    } else if (rate === "Rate B") {
+      if (timeOfUse === "Between noon and 6pm") {
+        if (bill_A < bill_B_high) {
+          return setMessage(message_A);
+        }
+        return setMessage(message_B);
+      }
+      if (timeOfUse === "Between 7pm and 11am") {
+        if (bill_A < bill_B_low) {
+          return setMessage(message_A);
+        }
+
+        return setMessage(message_B), { bill_B_low };
+      }
     }
     //   return (setMessageB(message_B));
 
@@ -81,13 +100,17 @@ const UserForm = () => {
     setRate("Rate A");
     setMiles("20000");
     setTimeOfUse("Between noon and 6pm");
+    setMessage("");
+    setSavings("")
+    setDifference("")
+   
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <UserRate handleRateChange={handleRateChange} rate={rate} />
-        
+
         <UserMiles handleMileChange={handleMileChange} miles={miles} />
         <UserTimeOfUse
           handleTOUChange={handleTOUChange}
@@ -96,8 +119,9 @@ const UserForm = () => {
         <button type='submit'>Submit</button>
       </form>
 
-      <h2>{message_A}</h2>
-      <h2>{message_B}</h2>
+      <h3>{message}</h3>
+      <h3>{savings}</h3>
+      <h2>{difference}</h2>
     </div>
   );
 };
