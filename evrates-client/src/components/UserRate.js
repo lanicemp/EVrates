1f416e9f1ad40d740e99a33cfa0ec8fc191887e3
LaNice <lanicemp10@gmail.com>
2021-04-05 17:15:58 -0400

    added div with classname for CSS

--- evrates-client/src/components/UserRate.js
+++ evrates-client/src/components/UserRate.js
@@ -1,12 +1,15 @@
 // import react from "react";
+import '../CSS/UserRate.css'
 
 const UserRate = ({ handleRateChange, rate }) => {
   //line 3 destructures handleRrateChange and HandleSubmit from props
 
   return (
     <div className="form-component">
+      <div className= "modal-question">
       <h3 >Which rate do you currently use?</h3>
-      <br></br>
+      </div>
+     
       <div className="rate-info">
       <h4>Rate A is a flat rate</h4>
       <h4> Rate B is based on your time of Use</h4>
